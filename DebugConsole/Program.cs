406fbd358601686120bbf33544a471e6f6443dc3
Dominik Janák <dev@dominikjanak.cz>
2020-03-03 21:58:32 +0100

    Add Debug Console application

---
+++ DebugConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using RangeTree;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RangeTreeData> data = new List<RangeTreeData>()
+            {
+                new RangeTreeData("K1", 5, 10),
+                new RangeTreeData("K2", 4, 10),
+                new RangeTreeData("K3", 8, 10),
+                new RangeTreeData("K4", 3, 10),
+                new RangeTreeData("K5", 9, 10)
+            };
+
+            RangeTree<RangeTreeData> tree = new RangeTree<RangeTreeData>();
+            tree.Build(data);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DebugConsole/RangeTreeData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugConsole
+{
+    class RangeTreeData : RangeTree.IData
+    {
+        private string _key;
+        private float _from;
+        private float _to;
+
+        public RangeTreeData(string key, float from, float to)
+        {
+            _key = key;
+            _from = from;
+            _to = to;
+        }
+
+        public string Key { get => _key; } 
+
+        public float GetFrom()
+        {
+            return _from;
+        }
+
+        public float GetTo()
+        {
+            return _to;
+        }
+    }
+}
